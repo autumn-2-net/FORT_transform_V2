43669f97beeadc487cc1ecc9562cf3c19091d45b
autumn <2>
2023-06-11 13:12:57 +0800

    att部分完成

--- base_modle/att_modle.py
+++ base_modle/att_modle.py
@@ -46,6 +46,119 @@ class attention(nn.Module):
         out = self.to_out(out)
         return self.dropout(out)
 
+@torch.jit.script
+def swish(x):
+    return x * torch.sigmoid(x)
+
+class GLU(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+        self.Lin1 = nn.Linear(dim,  dim)
+        self.Lin1 = nn.Linear(dim,  dim)
+
+    def forward(self, x):
+        out, gate = self.Lin1(x),self.Lin2(x)
+        return out * gate.sigmoid()
+class MLP_T1(nn.Module):
+    def __init__(self,dim,jhhc='GELU',dropout=0.0,pox=4):
+        super().__init__()
+        self.Lin1=nn.Linear(dim,pox*dim)
+
+
+        if jhhc=='GELU':
+            self.hc =nn.GELU()
+        elif jhhc == 'Swish':
+            self.hc = swish
+        elif jhhc == 'GLU':
+            self.hc = GLU(dim=dim)
+        else:
+            raise Exception('不支持方法')
+
+        self.Lin2 = nn.Linear( pox * dim,dim)
+        self.drp=nn.Dropout(dropout)
+    def forward(self, x):
+        x=self.Lin1(x)
+        x=self.hc(x)
+        x = self.Lin2(x)
+        return self.drp(x)
+
+
+class cross_att_lay(nn.Module):
+    def __init__(self, dim, heads, inner_dim, mlpdropout=0.0,pox=4, attdropout=0.0, att_type=None,jhhc='GELU'):
+        super().__init__()
+        self.attention_bh=attention(dim,heads,inner_dim,attdropout,att_type)
+        self.attention_img = attention(dim, heads, inner_dim, attdropout, att_type)
+
+
+        self.Mlp_bh=MLP_T1(dim,jhhc,mlpdropout,pox)
+        self.Mlp_img = MLP_T1(dim, jhhc, mlpdropout, pox)
+        self.ln_bh1=nn.LayerNorm(dim, eps=1e-12)
+        self.ln_bh2 = nn.LayerNorm(dim, eps=1e-12)
+        self.ln_img1 = nn.LayerNorm(dim, eps=1e-12)
+        self.ln_img1 = nn.LayerNorm(dim, eps=1e-12)
+
+    def forward(self, x_img, y_bh, bh_attention_mask=None, img_attention_mask=None):
+
+        y_bhs=self.ln_bh1(self.attention_bh(x_img,x_img,y_bh,bh_attention_mask)+y_bh)
+        y_bhs=self.ln_bh2(self.Mlp_bh(y_bhs)+y_bhs)
+
+        x_img = self.ln_bh1(self.attention_bh(y_bh, y_bh, x_img, img_attention_mask) + x_img)
+        x_img = self.ln_bh2(self.Mlp_bh(x_img) + x_img)
+
+        return x_img,y_bhs,
+
+class self_att_lay(nn.Module):
+    def __init__(self, dim, heads, inner_dim, mlpdropout=0.0,pox=4, attdropout=0.0, att_type=None,jhhc='GELU'):
+        super().__init__()
+        self.attention_bh=attention(dim,heads,inner_dim,attdropout,att_type)
+
+
+
+        self.Mlp_bh=MLP_T1(dim,jhhc,mlpdropout,pox)
+
+        self.ln_bh1=nn.LayerNorm(dim, eps=1e-12)
+        self.ln_bh2 = nn.LayerNorm(dim, eps=1e-12)
+
+
+    def forward(self, x, attention_mask=None):
+
+        x=self.ln_bh1(self.attention_bh(x,x,x,attention_mask)+x)
+        x=self.ln_bh2(self.Mlp_bh(x)+x)
+
+
+
+        return x
+class coross_attention(nn.Module):
+    def __init__(self,cross_lays, dim, heads, inner_dim, mlpdropout=0.0, attdropout=0.0,pox=4, att_type=None,jhhc='GELU'):
+        super().__init__()
+        self.lix=nn.ModuleList()
+        for i in range(cross_lays):
+            self.lix.append(cross_att_lay(dim,heads,inner_dim,mlpdropout,pox,attdropout,att_type,jhhc))
+
+    def forward(self, x_img, y_bh, bh_attention_mask=None, img_attention_mask=None):
+        for lay in self.lix:
+            y_bh, x_img=lay(x_img, y_bh, bh_attention_mask, img_attention_mask)
+        return y_bh,x_img
+
+class self_attention(nn.Module):
+    def __init__(self,lays, dim, heads, inner_dim, mlpdropout=0.0, attdropout=0.0,pox=4, att_type=None,jhhc='GELU'):
+        super().__init__()
+        self.lix=nn.ModuleList()
+        for i in range(lays):
+            self.lix.append(self_att_lay(dim,heads,inner_dim,mlpdropout,pox,attdropout,att_type,jhhc))
+
+    def forward(self, x, attention_mask=None):
+        for lay in self.lix:
+            x=lay(x,  attention_mask, )
+        return x
+
+
+
+
+
+
+
 
 
 
@@ -60,6 +173,8 @@ if __name__=='__main__':
 
     acs=ctc(key,value,query,attn)
     acs
+
+
     for i in range(100):
         asxd=random.randint(16,1024)
         asxds = random.randint(16, 1024)
@@ -71,6 +186,25 @@ if __name__=='__main__':
         value = torch.rand(asxdss, asxds, 512, dtype=torch.float32, device="cuda")
         acs = ctc(key, value, query, )
         print(acs.shape)
+
+    img = torch.rand(4, 80, 512, dtype=torch.float32, device="cuda")
+    bh = torch.ones(4, 42, 512, dtype=torch.float32, device="cuda")
+
+
+    aaa =cross_att_lay(512,8,512).cuda()
+    ccc=aaa(img,bh)
+    ccc
+
+    bbb=coross_attention(7,512,8,512).cuda()
+    dsds=bbb(img,bh)
+    dsds
+
+
+    bbbs=self_attention(7,512,8,512).cuda()
+    xdsdss=bbbs(img,)
+    xdsdss
+
+
 
 
 
